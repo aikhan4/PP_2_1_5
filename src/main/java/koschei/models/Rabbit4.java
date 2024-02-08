f281a06fb5a62414c954ee516497b2b6fd34c297
akasumv <akasum99@gmail.com>
2024-02-09 01:19:15 +0400

    решение (3)

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,9 +1,5 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Rabbit4 {
 
     private Duck5 duck5;
